c268f1f2cd8a20ee90e806df6378da1ab32f18cb
Mitansh Chaudhari <mitansh807@gmail.com>
2024-02-18 22:06:41 -0600

    Demo to Lab2 Setup is now complete

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Contact Database App', contactInfo: 'Mitansh Chaudhari, Email: mchaudhari1@hawk.iit.edu' });
 });
 
 module.exports = router;
